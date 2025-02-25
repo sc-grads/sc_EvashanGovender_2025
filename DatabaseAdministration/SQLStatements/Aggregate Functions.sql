45136a90abd20d008b404ae8c0a621c840288b2d
Evashan-Govender <evashan.govender@sambeconsulting.com>
2025-02-25 15:40:54 +0200

    Update Aggregate Functions.sql

--- DatabaseAdministration/SQLStatements/Aggregate Functions.sql
+++ DatabaseAdministration/SQLStatements/Aggregate Functions.sql
@@ -1,9 +1,2 @@
---Provide the sum,average,maximum and minimum of values given a cloumn
-SELECT SUM(TotalAmount) AS Revenue,
-		AVG(TotalAmount) AS AvgSaleAmt,
-		MAX(TotalAmount) AS HighestOrderAmt,
-		MIN(TotalAmount) AS LowestOrderAmt
-  FROM [PracticeDB].[dbo].[Orders],
 
---Provides the total number of records in a table
- --Select COUNT(*) AS TotalRecords FROM Orders 
+SELECT SUM(TotalAmount) AS Revenue,AVG(TotalAmount) AS AvgSaleAmt,MAX(TotalAmount) AS HighestOrderAmt,MIN(TotalAmount) AS LowestOrderAmt FROM Orders
