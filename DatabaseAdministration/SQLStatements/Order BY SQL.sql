5ab0cbd7ce59adc74fa8beb6cf192aabc93d4b30
Evashan-Govender <evashan.govender@sambeconsulting.com>
2025-02-25 15:18:40 +0200

    Update Order BY SQL.sql

--- DatabaseAdministration/SQLStatements/Order BY SQL.sql
+++ DatabaseAdministration/SQLStatements/Order BY SQL.sql
@@ -1,4 +1,4 @@
 
-SELECT FirstName,LastName,Email,ProductName,Category,Price,OrderDate,Quantity,TotalAmount 
+SELECT FirstName,LastName,Email,ProductName,Category,Price,OrderDate,Quantity,TotalAmount, 
 FROM Orders INNER JOIN Products INNER JOIN Orders ON Customers.CustomerID = Orders.CustomerID ON Products.ProductID = Orders.ProductID
 
