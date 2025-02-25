576dfa43afff551131eae06fe140717eee996a65
Evashan-Govender <evashan.govender@sambeconsulting.com>
2025-02-25 15:07:11 +0200

    Update Aggregate Functions.sql

--- DatabaseAdministration/SQLStatements/Aggregate Functions.sql
+++ DatabaseAdministration/SQLStatements/Aggregate Functions.sql
@@ -3,7 +3,6 @@ SELECT SUM(TotalAmount) AS Revenue,
 		AVG(TotalAmount) AS AvgSaleAmt,
 		MAX(TotalAmount) AS HighestOrderAmt,
 		MIN(TotalAmount) AS LowestOrderAmt
-	
   FROM [PracticeDB].[dbo].[Orders]
 
 --Provides the total number of records in a table
