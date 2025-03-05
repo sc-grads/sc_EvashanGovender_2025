8ed5e9216d224a28aae7877a09bbca21b7b2d4e5
unknown <evashangovender@sambeconsulting.com>
2025-03-05 21:28:29 +0200

    Simple python program to find the highest number given a list of numbers

--- Python/Find_Highest_Value.py
+++ Python/Find_Highest_Value.py
@@ -1,7 +1,9 @@
-maxNum = 0
+maxNum = None
 num = [22,34,55,44,32,756,43,76]
 for x in num:
-    if x > maxNum:
+    if maxNum == None:
+        maxNum = x
+    elif x > maxNum:
         maxNum = x
 
 print("Highest Number is: " ,maxNum)
