85645998005f90a7cca1a989f3621cf3ddc92f38
unknown <evashangovender@sambeconsulting.com>
2025-03-10 20:55:41 +0200

    Various examples SQL statements using the WHERE clause

---
+++ DatabaseAdministration/Udemy Course SQL Scripts/Select_using_WHERE_clause.sql
@@ -0,0 +1,11 @@
+USE AdventureWorks2022
+
+Select * FROM Person.Address Where PostalCode = '98011'
+
+Select * FROM Person.Address Where PostalCode != '98011'
+
+Select * FROM Person.Address Where ModifiedDate > '2008-12-19'
+
+
+--Various selects statements with the WHERE clause
+--Executes all Select statements and shows the results in like a spilt screen
