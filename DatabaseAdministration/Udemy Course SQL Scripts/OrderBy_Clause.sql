2db6f77cfb8fb5b2fd736d9261d2c5e9539842a5
unknown <evashangovender@sambeconsulting.com>
2025-03-10 21:26:21 +0200

    Various examples of the ORDER BY clause with conditions aswell

---
+++ DatabaseAdministration/Udemy Course SQL Scripts/OrderBy_Clause.sql
@@ -0,0 +1,9 @@
+USE AdventureWorks2022
+
+Select * FROM HumanResources.EmployeePayHistory ORDER BY Rate
+
+Select * FROM HumanResources.EmployeePayHistory ORDER BY Rate 
+
+Select * FROM HumanResources.EmployeePayHistory Where YEAR(ModifiedDate) = 2014 ORDER BY ModifiedDate,Rate
+
+--ORDER BY is default as Ascending or you can specify it as ORDER BY Rate ASC
