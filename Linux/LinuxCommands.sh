2704bbe7cf42231050cefbff8c5ffeab4dae47b2
unknown <evashangovender@sambeconsulting.com>
2025-04-01 15:01:11 +0200

    New commands added

--- Linux/LinuxCommands.sh
+++ Linux/LinuxCommands.sh
@@ -16,7 +16,8 @@ cp file.txt /backup # Copy file to /backup
 cp -r dir1 dir2     # Copy directory recursively  
 mv old.txt new.txt  # Rename file (or move to another location)  
 rm file.txt         # Delete file  
-rm -rf dir/         # Force-delete directory & contents (DANGEROUS!)  
+rm -rf dir/         # Force-delete directory & contents (DANGEROUS!) 
+
 
 # Viewing Files  
 cat file.txt        # Print entire file  
@@ -33,9 +34,54 @@ grep -r "TODO" ~/projects # Recursively search for "TODO"
 locate filename           # Find file using pre-built database (run `updatedb` first)
 
 #Permissions
-chmod 755 <file>  # Set permissions: user=rwx, group/others=rx
-chown user:group <file> # Change owner and group
-chgrp group <file> # Change group ownership only
+chmod 755 <file>            # Set permissions: user=rwx, group/others=rx
+chown user:group <file>     # Change owner and group
+chgrp group <file>          # Change group ownership only
+
+
+diff file1 file2            # Compare twofiles
+sdiff file1 file2           # Side by side comparison
+vimdiff file1 file2          # Highlight differences in vim
+
+#Customizing Shell Prompt
+/d                   # Date in "Weekday Month Date"
+/@                   # Current time in 12 hour format
+/h                   # Hostname up to the first period
+/H                   # Hostname
+/n                   # Newline
+/t                   #Current time in 24 hour HH:MM:SS format
+
+#Alias
+alias ll ='ls -l'    # Creates an alias for ls -l , now only need to type ll
+alias   	         # Lists the aliases
+unalias - a          # Deletes all aliases
+
+
+printenv             # Outputs all environment variables
+
+#Processes and Job Control
+ps -e               # Display all processes
+ps -ef              #Display all processes, full
+ps - eH             #Display a process tree
+ps -e --forest      #Display a process tree
+ps -u username      # Display user's processes
+pstree              #Display processes in a tree format
+command &           #Start command in background
+kill <PID>          # Terminate process by ID
+kill -9 <PID>       # Forcefully kill a process
+pkill <name>        # Kill process by name
+bg                  # Resume suspended job in background
+fg                  # Bring background job to foreground
+
+
+#Cron
+@yearly             # 0 0 1 1 *
+@annually           # 0 0 1 1 *
+@monthly            # 0 0 1 * *
+@weekly             # 0 0 * * 0
+@daily              # 0 0 * * *
+
+
 
 #System info
 uname -a     # Show all system/kernel info
@@ -60,10 +106,4 @@ sudo apt upgrade   # Upgrade installed packages
 sudo apt install <pkg> # Install a package
 sudo apt remove <pkg>  # Remove a package
 
-#Process Control
-kill <PID>          # Terminate process by ID
-kill -9 <PID>       # Forcefully kill a process
-pkill <name>        # Kill process by name
-bg                  # Resume suspended job in background
-fg                  # Bring background job to foreground
 
