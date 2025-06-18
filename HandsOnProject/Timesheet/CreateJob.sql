3a9153a3aa8d54758201d19ccd43955ba96b84a3
unknown <evashangovender@sambeconsulting.com>
2025-06-18 16:00:48 +0200

    Job script

--- HandsOnProject/Timesheet/CreateJob.sql
+++ HandsOnProject/Timesheet/CreateJob.sql
@@ -1,31 +1,44 @@
 USE [msdb]
 GO
-
--- 1. Create the job
-IF EXISTS (SELECT job_id FROM msdb.dbo.sysjobs WHERE name = N'Execute_SSIS_Package_Job')
-BEGIN
-    EXEC msdb.dbo.sp_delete_job @job_name = N'Execute_SSIS_Package_Job', @delete_unused_schedule = 1;
-END
-GO
-
 DECLARE @jobId BINARY(16)
-EXEC msdb.dbo.sp_add_job
-    @job_name = N'Execute_SSIS_Package_Job',
-    @enabled = 1,
-    @owner_login_name = N'sa', -- Change to appropriate login
-    @job_id = @jobId OUTPUT
-
--- 2. Add job step to execute the SSIS package
-EXEC msdb.dbo.sp_add_jobstep
-    @job_name = N'Execute_SSIS_Package_Job',
-    @step_name = N'Run SSIS Package',
-    @subsystem = N'SSIS',
-    @command = N'/FILE "\Integration Services Catalogs/SSISDB/SSISMigration/Projects/SSISProjectTrial/Packages/Packages.dtsx" /CHECKPOINTING OFF /REPORTING E',
-    @database_name = N'master'
-
- 
--- Assign the job to the target server
-EXEC msdb.dbo.sp_add_jobserver
-    @job_id = @jobId,
-    @server_name = N'(local)'; -- Adjust if targeting a remote server
+EXEC  msdb.dbo.sp_add_job @job_name=N'Execute_SSIS_Package_Job', 
+		@enabled=1, 
+		@notify_level_eventlog=0, 
+		@notify_level_email=2, 
+		@notify_level_page=2, 
+		@delete_level=0, 
+		@category_name=N'[Uncategorized (Local)]', 
+		@owner_login_name=N'Sambe2025002\EvashenGovender', @job_id = @jobId OUTPUT
+select @jobId
+GO
+EXEC msdb.dbo.sp_add_jobserver @job_name=N'Execute_SSIS_Package_Job', @server_name = N'SAMBE2025002\MSSQLSERVER2'
+GO
+USE [msdb]
+GO
+EXEC msdb.dbo.sp_add_jobstep @job_name=N'Execute_SSIS_Package_Job', @step_name=N'Step1', 
+		@step_id=1, 
+		@cmdexec_success_code=0, 
+		@on_success_action=1, 
+		@on_fail_action=2, 
+		@retry_attempts=0, 
+		@retry_interval=0, 
+		@os_run_priority=0, @subsystem=N'SSIS', 
+		@command=N'/ISSERVER "\"\SSISDB\SSISMigrationProject\SSISProjectTrial\Package.dtsx\"" /SERVER "\".\"" /Par "\"$ServerOption::LOGGING_LEVEL(Int16)\"";1 /Par "\"$ServerOption::SYNCHRONIZED(Boolean)\"";True /CALLERINFO SQLAGENT /REPORTING E', 
+		@database_name=N'master', 
+		@flags=0
+GO
+USE [msdb]
+GO
+EXEC msdb.dbo.sp_update_job @job_name=N'Execute_SSIS_Package_Job', 
+		@enabled=1, 
+		@start_step_id=1, 
+		@notify_level_eventlog=0, 
+		@notify_level_email=2, 
+		@notify_level_page=2, 
+		@delete_level=0, 
+		@description=N'', 
+		@category_name=N'[Uncategorized (Local)]', 
+		@owner_login_name=N'Sambe2025002\EvashenGovender', 
+		@notify_email_operator_name=N'', 
+		@notify_page_operator_name=N''
 GO
