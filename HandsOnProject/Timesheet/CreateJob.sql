9fc760d3698a763fcaa933c0678cc62e1442a305
unknown <evashangovender@sambeconsulting.com>
2025-06-18 11:47:59 +0200

    Job script

--- HandsOnProject/Timesheet/CreateJob.sql
+++ HandsOnProject/Timesheet/CreateJob.sql
@@ -2,11 +2,17 @@ USE [msdb]
 GO
 
 -- 1. Create the job
+IF EXISTS (SELECT job_id FROM msdb.dbo.sysjobs WHERE name = N'Execute_SSIS_Package_Job')
+BEGIN
+    EXEC msdb.dbo.sp_delete_job @job_name = N'Execute_SSIS_Package_Job', @delete_unused_schedule = 1;
+END
+GO
+
 DECLARE @jobId BINARY(16)
 EXEC msdb.dbo.sp_add_job
     @job_name = N'Execute_SSIS_Package_Job',
     @enabled = 1,
-    @owner_login_name = N'Sambe2025002\EvashenGovender', -- Change to appropriate login
+    @owner_login_name = N'sa', -- Change to appropriate login
     @job_id = @jobId OUTPUT
 
 -- 2. Add job step to execute the SSIS package
@@ -17,3 +23,9 @@ EXEC msdb.dbo.sp_add_jobstep
     @command = N'/FILE "\Integration Services Catalogs/SSISDB/SSISMigration/Project/Projects/SSISProjectTrial/Packages/Packages.dtsx" /CHECKPOINTING OFF /REPORTING E',
     @database_name = N'master'
 
+ 
+-- Assign the job to the target server
+EXEC msdb.dbo.sp_add_jobserver
+    @job_id = @jobId,
+    @server_name = N'(local)'; -- Adjust if targeting a remote server
+GO
