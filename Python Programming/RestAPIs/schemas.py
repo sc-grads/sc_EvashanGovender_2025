553961e2ede30d256707c418a2eb3ce9e008f34b
unknown <evashangovender@sambeconsulting.com>
2025-06-30 11:57:44 +0200

    Rest API Project files

--- Python Programming/RestAPIs/schemas.py
+++ Python Programming/RestAPIs/schemas.py
@@ -2,13 +2,18 @@ from marshmallow import Schema, fields
 
 
 class PlainItemSchema(Schema):
-    id = fields.Str(dump_only=True)
+    id = fields.Int(dump_only=True)
     name = fields.Str(required=True)
     price = fields.Float(required=True)
 
 class PlainStoreSchema(Schema):
-    id = fields.Str(dump_only=True)
+    id = fields.Int(dump_only=True)
     name = fields.Str(required=True)
+
+
+class PlainTagSchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.Str()
 
 class ItemUpdateSchema(Schema):
     name = fields.Str()
@@ -18,8 +23,26 @@ class ItemUpdateSchema(Schema):
 class ItemSchema(PlainItemSchema):
     store_id = fields.Int(required=True, load_only=True)
     store = fields.Nested(PlainStoreSchema, dump_only=True)
+    tags = fields.List(fields.Nested(PlainTagSchema()), dump_only=True)
 
 class StoreSchema(PlainStoreSchema):
     items = fields.List(fields.Nested(PlainItemSchema), dump_only=True)
+    tags = fields.List(fields.Nested(PlainTagSchema()), dump_only=True)
+
+class TagSchema(PlainTagSchema):
+    store_id = fields.Int(load_only=True)
+    store = fields.Nested(PlainStoreSchema(), dump_only=True)
+    items = fields.List(fields.Nested(PlainItemSchema()), dump_only=True)
+
+
+class TagAndItemSchema(Schema):
+    message = fields.Str()
+    tag = fields.Nested(TagSchema)
+    item = fields.Nested(ItemSchema)
+
+class UserSchema(Schema):
+    id = fields.Int(dump_only=True)
+    username = fields.Str(required=True)
+    password = fields.Str(required=True)
 
 
